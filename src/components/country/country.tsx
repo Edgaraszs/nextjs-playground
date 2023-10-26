27425cf330fb052ea0554c1e82fb0621c0f3554d
Edgaraszs <edgas258@gmail.com>
2023-10-26 22:41:18 +0300

    feat: add country component

---
+++ src/components/country/country.tsx
@@ -0,0 +1,15 @@
+interface Country {
+  capital: Array<any>
+};
+
+interface Props {
+  country: Country;
+};
+
+export default function Country({ country }: Props) {
+  return (
+    <div>
+      { country.capital.length > 0 && <span>Capital: {country.capital}</span> }
+    </div>
+  )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,17 @@
 import { useEffect, useState } from "react";
 import Dropdown from '@/components/dropdown/dropdown';
+import Country from '@/components/country/country';
 
 export default function Home() {
   const [countries, setCountries] = useState([]);
   const [anchor, setAnchor] = useState<HTMLDivElement | null>(null);
   const [countriesSelectVisible, setCountriesSelectVisibile] = useState(false);
+  const [country, setCountry] = useState({
+    capital: [],
+    name: {
+      common: '',
+    }
+  });
  
   const fetchCountries = async () => {
     const response = await fetch("/api/countries");
@@ -22,7 +29,7 @@ export default function Home() {
     const response = await fetch("/api/countries?countryName=" + value);
 
     const responseData = await response.json();
-    console.log(responseData.data);
+    setCountry(responseData.data[0]);
   }
 
   useEffect(() => {
@@ -30,26 +37,32 @@ export default function Home() {
   }, []);
 
   return (
-    <div>
-      <div className="container mx-auto mt-5 px-4">
-        <div ref={setAnchor}>
-          <label htmlFor="country_name" className="block mb-2 text-sm font-medium text-white">Country name</label>
-          <input
-            onClick={toogleCountriesSelect} 
-            type="text" 
-            id="country_name" 
-            className={`text-sm ${countriesSelectVisible ? 'rounded-b-none' : '' } rounded-lg w-full p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:outline-none focus:ring-1 focus:border-blue-400`}
-            placeholder="Select your country"
-          />
-          <Dropdown 
-            anchor={anchor} 
-            visible={countriesSelectVisible} 
-            items={countries}
-            toggleVisibility={toogleCountriesSelect}
-            selected={countrySelected}
-          />
+    <>
+      <div>
+        <div className="container mx-auto mt-5 px-4">
+          <div ref={setAnchor}>
+            <label htmlFor="country_name" className="block mb-2 text-sm font-medium text-white">Country name</label>
+            <input
+              onClick={toogleCountriesSelect} 
+              type="text" 
+              id="country_name" 
+              className={`text-sm ${countriesSelectVisible ? 'rounded-b-none' : '' } rounded-lg w-full p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:outline-none focus:ring-1 focus:border-blue-400`}
+              placeholder="Select your country"
+              defaultValue={country.name.common}
+            />
+            <Dropdown 
+              anchor={anchor} 
+              visible={countriesSelectVisible} 
+              items={countries}
+              toggleVisibility={toogleCountriesSelect}
+              selected={countrySelected}
+            />
+          </div>
+          <div className="mt-3">
+            <Country country={country}/> 
+          </div>
         </div>
       </div>
-    </div>
+    </>
   )
 }
